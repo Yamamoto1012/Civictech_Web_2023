bfad07e0f9bbd103eaa15b8584e280a64819b673
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-12-08 15:10:08 +0900

    Propsでアイコンを渡すようにした

--- ui/Header.jsx
+++ ui/Header.jsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import LinkButton from "./LinkButton";
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
+import ComputerOutlinedIcon from '@mui/icons-material/ComputerOutlined';
 
 export function Header() {
   return (
@@ -8,8 +10,8 @@ export function Header() {
         <h1 className="text-3xl font-bold text-gray-800 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">CivicTechWeb</h1>
       </Link>
       <div className="flex gap-4">
-        <LinkButton text="シビックテックについて" href="/about" />
-        <LinkButton text="事例" href="/case" />
+        <LinkButton text="シビックテックについて" href="/about" icon={<LibraryBooksOutlinedIcon />}/>
+        <LinkButton text="事例" href="/case" icon={<ComputerOutlinedIcon />} />
       </div>
     </header>
   );
--- ui/LinkButton.jsx
+++ ui/LinkButton.jsx
@@ -1,11 +1,10 @@
-import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
 import Link from 'next/link';
 
-export default function LinkButton({ text, href }) {
+export default function LinkButton({ text, href, icon }) {
   return (
     <Link href={href}>
       <button className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-black bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-        <LibraryBooksOutlinedIcon className="text-gray-500 mr-2"/>
+        {<span className="mr-2">{icon}</span>} 
         {text}
       </button>
     </Link>
