3107d2d542c6e3e2e8dcb08cf9bf7249ca28b4e2
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-12-15 15:20:34 +0900

    リンクの追加

--- ui/CaseContainer.jsx
+++ ui/CaseContainer.jsx
@@ -9,7 +9,7 @@ function CaseContainer() {
         <div className="h-auto md:h-[704px] w-full rounded-3xl shadow-md">
           <CaseTitle title="5374.jp" color="bg-[#FFC7C7]" />
           <div className="flex flex-col md:flex-row justify-center items-center mx-4 md:mx-20 mt-8 space-x-12 space-y-4 md:space-y-0 md:space-x-36">
-            <CasePicture picture="/5374.png" />
+            <CasePicture picture="/5374.png" link="http://5374.jp/"/>
             <CaseMainContext text="あああああああああああああああああああああああああああああああああああああああああああああああああああ" />
           </div>
         </div>
@@ -18,7 +18,7 @@ function CaseContainer() {
         <div className="h-auto md:h-[704px] w-full rounded-3xl shadow-md">
           <CaseTitle title="地域解決マッチング箱" color="bg-[#FFE2E2]" />
           <div className="flex flex-col md:flex-row justify-center items-center mx-4 md:mx-20 mt-8 space-x-12 space-y-4 md:space-y-0 md:space-x-36">
-            <CasePicture picture="/5374.png" />
+            <CasePicture picture="/5374.png" link="https://kanazawa.ha4go.net/"/>
             <CaseMainContext text="あああああああああああああああああああああああああああああああああああああああああああああああああああ" />
           </div>
         </div>
@@ -27,7 +27,7 @@ function CaseContainer() {
         <div className="h-auto md:h-[704px] w-full rounded-3xl shadow-md">
           <CaseTitle title="のとノットアローン" color="bg-[#8785A2]" />
           <div className="flex flex-col md:flex-row justify-center items-center mx-4 md:mx-20 mt-8 space-x-12 space-y-4 md:space-y-0 md:space-x-36">
-            <CasePicture picture="/5374.png" />
+            <CasePicture picture="/5374.png" link="http://noto.not-alone.jp/"/>
             <CaseMainContext text="あああああああああああああああああああああああああああああああああああああああああああああああああああ" />
           </div>
         </div>
--- ui/CasePicutre.jsx
+++ ui/CasePicutre.jsx
@@ -1,14 +1,17 @@
 import Image from "next/image"
+import Link from "next/link";
 
-function CasePicture({ picture }) {
+function CasePicture({ picture, link }) {
   return (
-    <div className="w-full max-w-[581px] md:w-[40%] h-[493px] relative shadow-lg rounded-md">
-      <Image 
-        src={picture} 
-        alt="case" 
-        layout="fill"
-        objectFit="contain"
-      />
+    <div className="w-full max-w-[581px] md:w-[40%] h-[493px] relative shadow-lg rounded-md transition-transform duration-150 ease-in-out transform hover:scale-105">
+      <Link href={link}>
+        <Image 
+          src={picture} 
+          alt="case" 
+          layout="fill"
+          objectFit="contain"
+        />
+      </Link>
     </div>
   );
 }
