33c72671823c0fc8f66f58d197a399c875e652a7
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-12-15 15:12:31 +0900

    レスポンシブ

--- ui/CaseContainer.jsx
+++ ui/CaseContainer.jsx
@@ -4,15 +4,35 @@ import CaseTitle from "./CaseTitle";
 
 function CaseContainer() {
   return (
-    <div className="p-4 md:p-8">
-      <div className="h-auto md:h-[704px] w-full rounded-3xl shadow-md">
-        <CaseTitle title="事例①" color="bg-[#FFC7C7]" />
-        <div className="flex flex-col md:flex-row justify-center items-center mx-4 md:mx-20 mt-8 space-x-12 space-y-4 md:space-y-0 md:space-x-36">
-          <CasePicture picture="/5374.png" />
-          <CaseMainContext text="aaaaaaaaa" />
+    <>
+      <div className="p-4 md:p-8">
+        <div className="h-auto md:h-[704px] w-full rounded-3xl shadow-md">
+          <CaseTitle title="5374.jp" color="bg-[#FFC7C7]" />
+          <div className="flex flex-col md:flex-row justify-center items-center mx-4 md:mx-20 mt-8 space-x-12 space-y-4 md:space-y-0 md:space-x-36">
+            <CasePicture picture="/5374.png" />
+            <CaseMainContext text="あああああああああああああああああああああああああああああああああああああああああああああああああああ" />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="p-4 md:p-8">
+        <div className="h-auto md:h-[704px] w-full rounded-3xl shadow-md">
+          <CaseTitle title="地域解決マッチング箱" color="bg-[#FFE2E2]" />
+          <div className="flex flex-col md:flex-row justify-center items-center mx-4 md:mx-20 mt-8 space-x-12 space-y-4 md:space-y-0 md:space-x-36">
+            <CasePicture picture="/5374.png" />
+            <CaseMainContext text="あああああああああああああああああああああああああああああああああああああああああああああああああああ" />
+          </div>
+        </div>
+      </div>
+      <div className="p-4 md:p-8">
+        <div className="h-auto md:h-[704px] w-full rounded-3xl shadow-md">
+          <CaseTitle title="のとノットアローン" color="bg-[#8785A2]" />
+          <div className="flex flex-col md:flex-row justify-center items-center mx-4 md:mx-20 mt-8 space-x-12 space-y-4 md:space-y-0 md:space-x-36">
+            <CasePicture picture="/5374.png" />
+            <CaseMainContext text="あああああああああああああああああああああああああああああああああああああああああああああああああああ" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- ui/Header.jsx
+++ ui/Header.jsx
@@ -1,18 +1,44 @@
+"use client";
 import Link from "next/link";
 import LinkButton from "./LinkButton";
-import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
-import ComputerOutlinedIcon from '@mui/icons-material/ComputerOutlined';
+import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import ComputerOutlinedIcon from "@mui/icons-material/ComputerOutlined";
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import { useState } from "react";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="flex justify-between items-center p-4 bg-white">
       <Link href="/">
-        <h1 className="text-xl md:text-3xl font-bold text-gray-800 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">CivicTechWeb</h1>
+        <h1 className="text-xl md:text-3xl font-bold text-gray-800 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          CivicTechWeb
+        </h1>
       </Link>
-      <div className="flex gap-2 md:gap-4">
+      <div className="hidden md:flex gap-4">
         <LinkButton text="シビックテックについて" href="/about" icon={<LibraryBooksOutlinedIcon />}/>
         <LinkButton text="事例" href="/case" icon={<ComputerOutlinedIcon />} />
       </div>
+
+      {/* ハンバーガーメニューアイコン */}
+      <div className="md:hidden">
+        <button onClick={() => setIsOpen(!isOpen)}>
+          <MenuOutlinedIcon className="h-6 w-6" />
+        </button>
+      </div>
+
+      {/* ナビゲーションメニュー */}
+      <div className={`fixed inset-y-0 right-0 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out bg-white shadow-lg p-4 z-50 md:hidden`}>
+        <button className="absolute top-4 right-4" onClick={() => setIsOpen(false)}>
+          <CloseOutlinedIcon className="h-6 w-6" />
+        </button>
+        <div className="flex-col flex mt-10">
+          <LinkButton text="シビックテックについて" href="/about" icon={<LibraryBooksOutlinedIcon />}/>
+          <LinkButton text="事例" href="/case" icon={<ComputerOutlinedIcon />} />
+        </div>
+      </div>
+
     </header>
   );
 }
